d32566829b799cfea7db1eafcf9192ddce960d23
GonzalezMelany <mela.glez9@gmail.com>
2026-03-20 16:19:16 -0700

    gg

--- blog/models.py
+++ blog/models.py
@@ -2,42 +2,43 @@ from django.db import models
 
 # Create your models here.
 class Category(models.Model):
-    title = models.CharField(max_lengh=225)
+    title = models.CharField(max_length=255)
 
     class Meta:
         ordering = ('title',)
-        verbose_name_plural = 'Categorias'
-
-        def _str_(self):
-            return self.title
-        
-class Post(models.Model):
-
-    ACTIVE = 'active'
-    DRAFT = 'draft'
-
-    CHOICES_STATUS = {
-        (ACTIVE, 'Active'),
-        (DRAFT, 'Draf')
-    }
-
-    category = models.ForeignKey(Category, related_name='posts', on_delete=models.CASCADE)
-    title = models.CharField(max_length=255)
-    intro = models.TextField()
-    body = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    status = models.CharField(max_length=10, choices=CHOICES_STATUS, default=ACTIVE)
-    image = models.ImageField(upload_to='upload/', blank=True, null=True)
+        verbose_name_plural = 'Categories'
 
     def __str__(self):
         return self.title
     
-class comment (models.Model):
-    post  = models.ForeignKey(Post, related_name='Comments', on_delete=models.CASCADE)
-    name = models.CharField(max_length=255)
-    email = models.EmailField()
-    body = models.EmailField()
+class Post(models.Model):
+
+    ACTIVATE = 'active'
+    DRAFT = 'draft'
+
+    CHOICES_STATUS = {
+        (ACTIVATE, 'Active'),
+        (DRAFT, 'Draft')
+    }
+
+    category = models.ForeignKey(Category, related_name='posts', on_delete=models.CASCADE)
+    title  = models.CharField(max_length=255)
+    intro = models.TextField()
+    body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=10, choices=CHOICES_STATUS, default=ACTIVATE)
+    image = models.ImageField(upload_to='upload/', blank=True, null=True)
 
     def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    name  = models.CharField(max_length=255)
+    email = models.EmailField()
+    body = models.TextField()
+    created_ad = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
         return self.name
+    
